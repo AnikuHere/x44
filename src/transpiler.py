7d3721dcee085bd3effc3627e6cd8106c8e5a3e2
AnikuHere <lemonjavamate7@gmail.com>
2026-01-03 22:10:13 +0400

    Enhance X44Transpiler with new commands
    Updated the transpiler to include additional commands and improve functionality, as well as some of the original SC5 commands that somehow didnt get ported to GTHB

--- src/transpiler.py
+++ src/transpiler.py
@@ -12,7 +12,7 @@ class X44Transpiler:
             "#pragma comment(lib, \"user32.lib\")",
             "#pragma comment(lib, \"ws2_32.lib\")",
             "int main() {",
-            '    std::cout << "--- X44 MASTER ONLINE ---" << std::endl;'
+            '    std::cout << "--- X44 FULL PROTOCOL ACTIVE ---" << std::endl;'
         ]
 
     def translate(self):
@@ -21,20 +21,53 @@ class X44Transpiler:
                 for line in f:
                     line = line.strip()
                     if not line or line.startswith("--"): continue
-                    p = line[0]
-                    c = line[1:].strip()
+                    p = line[0] # Prefix
+                    c = line[1:].strip() # Content
 
+                    # [_] Logging
                     if p == "_":
                         self.cpp_code.append('    std::cout << "[KERNEL] ' + c.replace('"', '') + '" << std::endl;')
+                    
+                    # [$] Popup
                     elif p == "$":
                         self.cpp_code.append('    MessageBoxA(NULL, "' + c.replace('"', '') + '", "X44", 0x30);')
+                    
+                    # [!] Alarm
                     elif p == "!":
-                        self.cpp_code.append('    Beep(800, 200);')
+                        self.cpp_code.append('    Beep(800, 200); Beep(1000, 200);')
+                    
+                    # [@] Input/Key Wait
                     elif p == "@":
                         self.cpp_code.append('    while(!(GetAsyncKeyState(\'' + c.replace('"', '') + '\') & 0x8000)) { Sleep(10); }')
+                    
+                    # [>] Voice TTS
                     elif p == ">":
                         v = 'powershell -Command "Add-Type -AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak(\'' + c.replace('"', '') + '\');"'
                         self.cpp_code.append('    system("' + v.replace('"', '\\"') + '");')
+
+                    # [&] Web Download
+                    elif p == "&":
+                        self.cpp_code.append('    system("curl -L ' + c.replace('"', '') + ' -o x44_download.txt");')
+
+                    # [*] Firewall
+                    elif p == "*":
+                        fw = 'netsh advfirewall firewall add rule name=\\"X44_Block\\" dir=in action=block remoteip=' + c.replace('"', '')
+                        self.cpp_code.append('    system("' + fw + '");')
+
+                    # [-] Draw UI Line
+                    elif p == "-":
+                        self.cpp_code.append('    std::cout << "========================================" << std::endl;')
+
+                    # [+] Perform/Execute App
+                    elif p == "+":
+                        self.cpp_code.append('    system("start ' + c.replace('"', '') + '");')
+
+                    # [%] Screenshot
+                    elif p == "%":
+                        ps_cam = 'Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.SendKeys]::SendWait(\'{PRTSC}\'); Start-Sleep -m 500; $img = [System.Windows.Forms.Clipboard]::GetImage(); $img.Save(\'snap.png\', [System.Drawing.Imaging.ImageFormat]::Png)'
+                        self.cpp_code.append('    system("powershell -Command \\"' + ps_cam + '\\"");')
+
+                    # [?] Self-Destruct
                     elif p == "?":
                         self.cpp_code.append('    system("start /b \\\"\\\" cmd /c timeout /t 2 & del /f /q X44_App.exe");')
 
@@ -42,7 +75,7 @@ class X44Transpiler:
             out = self.source_file.replace(".x44", ".cpp")
             with open(out, 'w', encoding='utf-8') as f_out:
                 f_out.write("\n".join(self.cpp_code))
-            print("SUCCESS: Generated " + out)
+            print("SUCCESS: Full Chaos Version Generated " + out)
         except Exception as e:
             print("ERROR: " + str(e))
 
